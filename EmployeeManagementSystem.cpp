f1427908da1af3cf470c56bc1f2dae0e29413685
Vijay <uddarrajubharath@gmail.com>
2023-10-09 23:29:41 +0530

    Add project files.

---
+++ EmployeeManagementSystem.cpp
@@ -0,0 +1,12 @@
+﻿// EmployeeManagementSystem.cpp : Defines the entry point for the application.
+//
+
+#include "EmployeeManagementSystem.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello Bharath." << endl;
+	return 0;
+}
